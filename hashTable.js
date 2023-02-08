83bcca2da6e7c5e26e3284a70bb2e208d9b56f4b
tylerschlepko <97323708+tylerschlepko@users.noreply.github.com>
2023-02-08 14:30:06 -0600

    Binary Search Tree Breadth and depth search done

--- hashTable.js
+++ hashTable.js
@@ -14,17 +14,44 @@ class HashTable {
 
     set(key, value) {
         const index = this.hash(key)
-        this.table[index] = value
+        //this.table[index] = value
+        const bucket = this.table[index]
+        if(!bucket){
+            this.table = [[key, value]]
+        } else {
+            const sameKeyItem = bucket.find(item => item[0] === key)
+            if(sameKeyItem) {
+                sameKeyItem[1] = value
+            } else {
+                bucket.push([key, value])
+            }
+        }
+
     }
 
     get(key) {
         const index = this.hash(key)
-        return console.log(this.table[index])
+        //return console.log(this.table[index])
+        const bucket = this.table[index]
+        if(bucket){
+            const sameKeyItem = bucket.find(item => item[0] === key)
+            if(sameKeyItem){
+                return sameKeyItem[1]
+            }
+        }
+        return undefined
     }
 
     remove(key) {
         const index = this.hash(key)
-        this.table[index] = undefined
+        //this.table[index] = undefined
+        const bucket = this.table[index]
+        if(bucket){
+            const sameKeyItem = bucket.find(item => item[0] === key)
+            if(sameKeyItem){
+                bucket.splice(bucket.indexOf(sameKeyItem), 1)
+            }
+        }
     }
 
     display() {
